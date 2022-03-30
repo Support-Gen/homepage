7360a3249d70b3a1fe2fdb39ef6b10ba4add9673
MHuesler <hueslermaxim@gmail.com>
2022-03-30 23:49:38 +0200

    added founders note component

---
+++ components/founders-note.tsx
@@ -0,0 +1,12 @@
+import Reveal from "react-awesome-reveal";
+import { fadeInBottom } from "../styles/animations";
+
+const FoundersNote = () => {
+  return (
+    <section className='container mx-auto px-4 flex flex-row relative'>
+      
+    </section>
+  )
+}
+
+export default FoundersNote;
