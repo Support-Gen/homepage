d41c658be4942dcc87b7d1e8e87f01cc06369cfa
MHuesler <hueslermaxim@gmail.com>
2022-05-03 23:36:54 +0200

    minors

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className='mt-24 text-center'>
           <h1 className='font-bold text-5xl md:text-7xl text-slate-900 mb-10'>Product docs <br></br>that <span className='font-extrabold underline decoration-wavy text-indigo-500'>save you time</span></h1>
           <h2 className='font-medium text-lg px-5 text-slate-600 mb-14 lg:w-1/2 xl:w-2/5 mx-auto'>We generate a self-service documentations out of your web-apps, that make your customer&apos;s and your life easier.</h2>
-          <button className="text-lg font-medium bg-violet-600 hover:bg-violet-500 text-white rounded-2xl px-10 py-3 mb-2 shadow-md shadow-indigo-300">Start generating docs</button>
+          <button className="text-lg font-medium bg-indigo-600 hover:bg-indigo-500 text-white rounded-2xl px-10 py-3 mb-2 shadow-md shadow-indigo-300">Start generating docs</button>
           <p className="font-normal text-sm">money back guarantee</p>
         </div>
       </Reveal>
